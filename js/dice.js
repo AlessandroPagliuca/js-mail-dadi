ae3893f0b635ea1de6446adf4e7c0f075e74f7f0
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-03-13 16:29:43 +0100

    Upgrade dice

--- js/dice.js
+++ js/dice.js
@@ -3,3 +3,13 @@
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
  */
+
+// Create array for users name 
+
+let usersName = ['Liam', 'Lucas', 'James', 'Oliver', 'Elijah'];
+console.log(usersName);
+
+//create random number
+
+let getRandomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(getRandomNumber);
